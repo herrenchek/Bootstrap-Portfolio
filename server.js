4ae13cb48e527c3408b2783a6240d122260b186c
herrenchek <giselle.herr@icloud.com>
2019-09-19 23:06:14 -0400

    Styles function hooray

--- server.js
+++ server.js
@@ -13,20 +13,20 @@ var express = require("express");
 // Tells node that we are creating an "express" server
 var app = express();
 
-// Sets an initial port. We"ll use this later in our listener
+// Sets an initial port to be used in listener.
 var PORT = process.env.PORT || 8080;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("/public"));
+app.use(express.static("app/public"));
 
 // ================================================================================
 // ROUTER
 // The below points our server to a series of "route" files.
 // ================================================================================
 
-require("./routes/htmlRoutes")(app);
+require("./app/routes/htmlRoutes")(app);
 
 // =============================================================================
 // LISTENER
